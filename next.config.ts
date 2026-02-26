7eb0a924ef626062327e6709122b60acfabb9759
gowri c <gowri@baboolsoft.com>
2026-02-26 16:02:30 +0530

    outstatic update

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  env: {
+    OST_GITHUB_ID: "Ov23liH2P2Ls0YZ4RKMa",
+    OST_GITHUB_SECRET: "f6dfc2e5a359169e6e127c0dfda1974a000037f5",
+    OST_REPO_SLUG: "outstaic_blogs",
+    NEXTAUTH_SECRET: "your-random-super-secret-key-123456789",
+  }
 };
 
 export default nextConfig;
